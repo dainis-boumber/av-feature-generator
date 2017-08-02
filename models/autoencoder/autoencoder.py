f0ea3a143e4760ec58411f818fdd8b668119b441
Dainis Boumber <dainis.boumber@gmail.com>
2017-08-02 00:04:32 -0500

    Updating autoencoder, tested dockerfile

--- models/autoencoder/autoencoder.py
+++ models/autoencoder/autoencoder.py
@@ -1,7 +1,20 @@
 import tensorflow as tf
-from tensorflow.models.autoencoder.autoencoder_models.DenoisingAutoencoder import MaskingNoiseAutoencoder
+from enum import Enum
+from tensorflow.models.autoencoder.autoencoder_models.DenoisingAutoencoder import
 
-class AutoEncoderFactory:
+class StackedAutoEncoder():
 
-    def __init__(self, BaseAutoEncoder):
-        self.ae = 
+    def __init__(self, noise_type='masking'):
+        self.noise_type = noise_type
+        self.ae = None
+        if noise_type = 'none':
+            self.ae = Autoencoder()
+        elif noise_type = 'gaussian':
+            self.ae = GaussianNoiseAutoencoder()
+        elif noise_type = 'masking':
+            self.ae = MaskingNoiseAutoencoder()
+        elif noise_type = 'variational':
+            self.ae = VariationalAutoencoder()
+        else:
+            raise NameError
+
