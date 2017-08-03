ebf25cb2c1cb0dddedfcc936d956cfe1d2b2a483
aeryen <aeryen@gmail.com>
2017-08-03 01:51:09 -0500

    add a data helper from someone else's github

---
+++ WordDict.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from collections import Counter
+
+
+def dictFromFile(aFileName):
+    with open(aFileName) as inFile:
+        listsOfWords = inFile.read().split()
+    return Counter(listsOfWords)
+
+
+def getListListPANAndFoldersPAN(aPath):
+    aListListPAN = []
+    foldersPAN = []
+
+    for d in os.listdir(aPath):
+        aListPAN = []
+        if os.path.isfile(aPath + "/" + d):
+            continue
+        foldersPAN.append(d)
+        for f in os.listdir(aPath + "/" + d + "/"):
+            aListPAN.append(aPath + "/" + d + "/" + f)
+        aListListPAN.append(sorted(aListPAN))
+    foldersPAN, aListListPAN = zip(*sorted(zip(foldersPAN, aListListPAN)))
+
+    return aListListPAN, foldersPAN
