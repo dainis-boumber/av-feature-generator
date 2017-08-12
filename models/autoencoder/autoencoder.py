89504198676f1214c52b7507df926a81a6d9e042
Dainis Boumber <dainis.boumber@gmail.com>
2017-08-12 05:46:49 -0500

    Added autoencoder models

--- models/autoencoder/autoencoder.py
+++ models/autoencoder/autoencoder.py
@@ -1,20 +1,22 @@
-import tensorflow as tf
-from enum import Enum
-import
+import tensorflow
+import base.autoencoder_models.AdditiveGaussianNoiseAutoencoder as GaussianNoiseAutoencoder
+from base.autoencoder_models import Autoencoder, VariationalAutoencoder
+from base.autoencoder_models.DenoisingAutoencoder import MaskingNoiseAutoencoder
+
 
 class StackedAutoEncoder():
 
     def __init__(self, noise_type='masking'):
         self.noise_type = noise_type
         self.ae = None
-        if noise_type = 'none':
+        if noise_type is None:
             self.ae = Autoencoder()
-        elif noise_type = 'gaussian':
+        elif noise_type == 'gaussian':
             self.ae = GaussianNoiseAutoencoder()
-        elif noise_type = 'masking':
+        elif noise_type == 'masking':
             self.ae = MaskingNoiseAutoencoder()
-        elif noise_type = 'variational':
+        elif noise_type == 'variational':
             self.ae = VariationalAutoencoder()
         else:
-            raise NameError
+            raise ValueError
 
