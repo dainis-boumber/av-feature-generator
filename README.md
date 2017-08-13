642fbd5423be988bb261d44b667456ee30afbc1f
Dainis Boumber <dainis.boumber@gmail.com>
2017-08-13 08:11:38 -0500

    Preprocessing to dictionary added

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # av-feature-generator
 Feature generator for Authorship Verification problems with pair-wise data
 
-Documentation can be found on [Project Wiki](https://github.com/dainis-boumber/av-feature-generator/wiki)
+After preprocess.py initializes the dataset, the format for each split is like:
+
+```
+{ "EN001" : [{"known" : ["/absolute/path/to/known.txt"], "unknown" : ["/absolute/path/to/unknown.txt"], "label" : True/False]}  
+```
+
+More documentation can be found on [Project Wiki](https://github.com/dainis-boumber/av-feature-generator/wiki)
--- utils/preprocess.py
+++ utils/preprocess.py
@@ -8,5 +8,27 @@ class PAN:
 
     def __init__(self, year):
         p = os.path.abspath(__file__ + "/../../data/PAN" + str(year) + '/')
-        ll = getListListPANAndFoldersPAN(p)
+        pair_dirs, self.splits = getListListPANAndFoldersPAN(p)
+        self.data = {}
+
+        for pair_dir in pair_dirs:
+            _ , tail = os.path.split(pair_dir)
+            description = []
+            if 'train' in pair_dirs:
+                with open('truth.txt') as truth:
+                    for line in truth:
+                        description.append({"label" : line.strip().split()[1]})
+            for path in os.listdir(pair_dir):
+                if not os.path.isfile(path): raise TypeError
+                pair = {}
+                if 'unknown' in path:
+                    pair.update({'unknown' : path})
+                else:
+                    pair.update({'known' : path})
+                description.append(pair)
+            self.data.update({tail : description})
+
+
+
+        pass
 
